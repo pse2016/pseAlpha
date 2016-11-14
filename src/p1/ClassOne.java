e3cb8fdb8b4fe12d4a5c05f9a33b285a4abc2b04
Nephalem <Nephalem@DESKTOP-O5T6L61>
2016-11-14 21:00:21 +0100

    second commit

--- src/p1/ClassOne.java
+++ src/p1/ClassOne.java
@@ -3,5 +3,6 @@ package p1;
 public class ClassOne {
 	public static void main(String[] args) {
 		System.out.println("Hello from class 1 BITCH!");
+		System.out.println("Hello from user 2 BITCH!");
 	}
 }
