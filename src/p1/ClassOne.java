6cd2e89bde43c034649a4448800bef1f6d9c70c6
Nephalem <Nephalem@DESKTOP-O5T6L61>
2016-11-14 21:04:24 +0100

    greets from user 1 beach

--- src/p1/ClassOne.java
+++ src/p1/ClassOne.java
@@ -4,5 +4,6 @@ public class ClassOne {
 	public static void main(String[] args) {
 		System.out.println("Hello from class 1 BITCH!");
 		System.out.println("Hello from user 2 BITCH!");
+		System.out.println("Hello from user 1 BEACH!");
 	}
 }
